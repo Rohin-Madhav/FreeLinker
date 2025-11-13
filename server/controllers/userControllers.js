ae167ad6eb32fdb3293bde4cdabc0d350b458783
Rohin Madhav <rohinmadhavk7@gmail.com>
2025-11-13 18:24:08 +0530

    small change

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -25,7 +25,7 @@ exports.getAllUsers = async (req, res) => {
 exports.getUserById = async (req,res) =>{
   try {
     const {id} = req.params
-    const user = await User.findById(id)
+     const user = await User.findById(id).select("-password");
     if(!user){
       return res.status(404).json({ message: "User not found" }); 
     }
